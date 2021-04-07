0173a31351c8a6b219f56fcd7eb0f394ec93fcc0
Ali <alihaider@placemarktechnologies.com>
2021-04-07 20:54:27 +0500

    update db url

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         database: env('DATABASE_NAME'),
-        uri: `mongodb://${env('DATABASE_USERNAME')}:${env('DATABASE_PASSWORD')}@${env('DATABASE_HOST')}:${env('DATABASE_PORT')}/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000`,
+        uri: `mongodb+srv://${env('DATABASE_USERNAME')}:${env('DATABASE_PASSWORD')}@${env('DATABASE_HOST')}/${env('DATABASE_NAME')}?retryWrites=true&w=majority`,
       },
       options: {
         ssl: true,
